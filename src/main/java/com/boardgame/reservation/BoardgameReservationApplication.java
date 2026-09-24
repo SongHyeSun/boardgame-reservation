fc1acd07bbcdcefc73ec957f925f40c30122cbc5
SongHyeSun <kate__@naver.com>
2026-09-25 00:19:57 +0900

    chore: 프로젝트 초기 세팅 (Spring Boot + PostgreSQL + Redis)

---
+++ src/main/java/com/boardgame/reservation/BoardgameReservationApplication.java
@@ -0,0 +1,13 @@
+package com.boardgame.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgameReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardgameReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boardgame/reservation/BoardgameReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardgame.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgameReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
